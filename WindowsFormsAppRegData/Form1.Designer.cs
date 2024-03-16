ee9189865ecabee4e01f27b70d51e023d4f5a2b3
Mujo Didic <mujocsharpdev@gmail.com>
2024-03-16 08:49:51 +0100

    Add project files.

---
+++ WindowsFormsAppRegData/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsAppRegData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ButtonLesen = new System.Windows.Forms.Button();
+            this.ButtonSchreiben = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(354, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // ButtonLesen
+            // 
+            this.ButtonLesen.Location = new System.Drawing.Point(24, 170);
+            this.ButtonLesen.Name = "ButtonLesen";
+            this.ButtonLesen.Size = new System.Drawing.Size(104, 27);
+            this.ButtonLesen.TabIndex = 2;
+            this.ButtonLesen.Text = "Lesen";
+            this.ButtonLesen.UseVisualStyleBackColor = true;
+            this.ButtonLesen.Click += new System.EventHandler(this.ButtonLesen_Click);
+            // 
+            // ButtonSchreiben
+            // 
+            this.ButtonSchreiben.Location = new System.Drawing.Point(274, 170);
+            this.ButtonSchreiben.Name = "ButtonSchreiben";
+            this.ButtonSchreiben.Size = new System.Drawing.Size(104, 27);
+            this.ButtonSchreiben.TabIndex = 3;
+            this.ButtonSchreiben.Text = "ButtonSchreiben";
+            this.ButtonSchreiben.UseVisualStyleBackColor = true;
+            this.ButtonSchreiben.Click += new System.EventHandler(this.ButtonSchreiben_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 243);
+            this.Controls.Add(this.ButtonSchreiben);
+            this.Controls.Add(this.ButtonLesen);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ButtonLesen;
+        private System.Windows.Forms.Button ButtonSchreiben;
+    }
+}
+
---
+++ WindowsFormsAppRegData/Form1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppRegData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLesen_Click(object sender, EventArgs e)
+        {
+            //den Schlüssel
+            //HKEY_CURRENT_USER\Software\RegistryDemo öffen
+
+            using (RegistryKey regSchluessel = Registry.CurrentUser.OpenSubKey("Software\\RegistryDemo"))
+            {
+                //wenn der Schlüssel nicht vorhanden ist,eine Meldung ausgeben
+                if (regSchluessel == null)
+                {
+                    MessageBox.Show("Der Schlüssel konnte nicht geöffner werden!");
+                    label1.Text = "Nicht vorhanden!";
+                }
+                else
+                    //sonst den Eintrag lesen und label1 zuweisen
+                    label1.Text = Convert.ToString(regSchluessel.GetValue("Eintrag"));
+
+            }
+        }
+
+        private void ButtonSchreiben_Click(object sender, EventArgs e)
+        {
+            //den Schlüssel HKEY_CURRENT_USER\Software\RegistryDemo
+            //anlegen bzw. öffen
+
+            using (RegistryKey regSchluessel = Registry.CurrentUser.CreateSubKey("Software\\RegistryDemo"))
+            {
+                //ddern Wertaus dem Eingabrfekd in den Eintrag schreiben
+                regSchluessel.SetValue("Eintrag", textBox1.Text);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //beim Schließen speichern wir die Position und die Große in der Registrierung
+            //den Schlüßel HKEY_CURRENT_USER\Software\RegistryDemo 
+            //anlegen bzw. öffen
+
+            using (RegistryKey regSchluessel = Registry.CurrentUser.CreateSubKey("Software\\RegistryDemo"))
+            {
+                regSchluessel.SetValue("Top", this.Top);
+                regSchluessel.SetValue("Left", this.Left);
+                regSchluessel.SetValue("Height", this.Height);
+                regSchluessel.SetValue("Width", this.Width);
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //beim Anzeigen lesen wir die Daten wieder zurück
+            //den Schlüssel HKEY_CURRENT_USER\Software\RegistryDemo öffen
+
+            using (RegistryKey regSchluessel = Registry.CurrentUser.OpenSubKey("Software\\RegistryDemo"))
+            {
+                //wenn der Schlüßel nicht vorhanden ist,Meldung ausgeben
+                if (regSchluessel == null)
+                {
+                    MessageBox.Show("Der Schlüssel konnte nicht geöffnet werden!");
+                }
+                else
+                {
+                    //sonst dei Einträge lesen und zuwiesen
+                    this.Top = Convert.ToInt32(regSchluessel.GetValue("Top"));
+                    this.Left = Convert.ToInt32(regSchluessel.GetValue("Left"));
+                    this.Height = Convert.ToInt32(regSchluessel.GetValue("Height"));
+                    this.Width = Convert.ToInt32(regSchluessel.GetValue("Width"));
+                }
+            }
+        }
+    }
+}
